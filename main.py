18595398c59cc42ec177c474567decb8bfffa445
iohnS <john02sun@gmail.com>
2021-12-03 18:24:04 +0100

    final version of api

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
-from flask import Flask, jsonify, make_response
-from flask.templating import render_template
+from flask import Flask, json, jsonify, make_response
+#from flask.templating import render_template
 from flask_restful import Api, Resource
 from schedule import Schedule
 
@@ -9,19 +9,22 @@ app = Flask(__name__)
 api = Api(app)
 
 schedule = Schedule()
-schedules = schedule.getSchedule()
+
 
 @app.route('/', methods=['GET'])
 def home():
-    return schedule.getSchedule()
-    
-@app.route('/test')
-def test():
-    return {"test" : ["this", "is", "a", "test"]}
+    return schedule.getToday("20211203")
+
 
 @app.route('/api/schedule', methods=['GET'])
-def api():
-    return jsonify(schedules)
+def scheduleApi():
+    return jsonify(schedule.getSchedule())
+
+
+@app.route('/api/agenda/<date>', methods=['GET'])
+def agendaAPI(date):
+    return jsonify(schedule.getToday(date))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- schedule.py
+++ schedule.py
@@ -1,11 +1,8 @@
 from flask import jsonify
 from flask.sessions import SecureCookieSession
 
-class Schedule:
 
-    def __init__(self):
-        self.schedules = {}
-        self.blockList = []
+class Schedule:
 
     def getAgenda(self, currentDate):
         from bs4 import BeautifulSoup
@@ -18,7 +15,7 @@ class Schedule:
 
         currentBlock = soup.find('div', {"data-dates": f"{currentDate}"})
         if currentBlock is not None:
-            courses = currentBlock.find_all("div", {"tabindex" : "0"})
+            courses = currentBlock.find_all("div", {"tabindex": "0"})
             blockTimes = currentBlock.find_all("div", {"class": "timeDiv"})
 
         # course1:
@@ -29,28 +26,27 @@ class Schedule:
         #     stutentgroup -> D2
         #     lärare -> björn
 
+            blockInfo = {0: "course",
+                         1: "lectureType",
+                         3: "room",
+                         4: "class",
+                         5: "studentgrupp",
+                         6: "teacher",
+                         7: "startTime",
+                         8: "endTime"
+                         }
 
-
-            blockInfo = {0: "course", 
-                        1: "lectureType",
-                        3: "room",
-                        4: "class",
-                        5: "studentgrupp",
-                        6: "teacher",
-                        7: "startTime",
-                        8: "endTime"
-                        }
-
-            #Reads the block info for each of the classes
+            # Reads the block info for each of the classes
             courseNbr = 0
-            
+
             for info in courses:
-                courseNbr+=1
-                blockDict = {} 
+                courseNbr += 1
+                blockDict = {}
                 #agenda[f"course{courseNbr}"] = {}
                 for i in [0, 1, 3, 4, 5, 6]:
-                    for value in info.find_all("div", {"class" : f"c col{i}"}):
-                        blockDict[blockInfo[i]] = value.string if value != None else ""
+                    for value in info.find_all("div", {"class": f"c col{i}"}):
+                        blockDict[blockInfo[i]
+                                  ] = value.string if value != None else ""
                 end = True
                 for times in blockTimes[courseNbr*2-2:courseNbr*2]:
                     if end:
@@ -63,18 +59,23 @@ class Schedule:
                 agenda.append(blockDict)
         return agenda
 
+    def getToday(self, currentDate):
+        agenda = {}
+        agenda["agenda"] = self.getAgenda(currentDate)
+        return agenda
+
     def getSchedule(self):
         from datetime import date, timedelta
-        
+
         today = date.today()
-        allDays = [today + timedelta(days = i) for i in range(0,7)]
-        schedules = self.schedules
-        blockList = self.blockList
+        allDays = [today + timedelta(days=i) for i in range(0, 7)]
+        schedules = {}
+        blockList = []
 
         for day in allDays:
             currentDay = day.strftime("%Y%m%d")
             blockList.append(self.getAgenda(currentDay))
-            
+
         schedules["schedule"] = blockList
 
-        return schedules;
+        return schedules
